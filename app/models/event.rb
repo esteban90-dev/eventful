4e125d9366e449177f1e0a90f27ca4fd59c27c43
Stephen Allard <stephencallard@gmail.com>
2020-09-12 09:48:34 -0500

    Add assocations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+  belongs_to :host, class_name: "User"
+  has_many :invitations
+  has_many :attendees, through: :invitations
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :hosted_events, foreign_key: "host_id", class_name: "Event"
+  has_many :invitations, foreign_key: "attendee_id"
+  has_many :attending_events, through: :invitations, source: "event"
 end
