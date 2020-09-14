c0b521d361c0f7b05a40aa12ff433cf69008e3ea
Stephen Allard <stephencallard@gmail.com>
2020-09-14 17:55:30 -0500

    Create should redirect to Events#index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash.notice = "A new user has been created"
-      #redirect_to
+      redirect_to events_path
     else
       render :new
     end
