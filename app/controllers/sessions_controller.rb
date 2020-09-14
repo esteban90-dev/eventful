961e41e7b3390d3f2c184993a9541e48a338adf4
Stephen Allard <stephencallard@gmail.com>
2020-09-14 18:40:56 -0500

    Add #new, #create, and #destroy to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:current_user_id] = user.id 
+      flash[:notice] = "Signed in successfully!"
+      redirect_to events_path
+    else
+      flash.now[:notice] = "Email or password is incorrect."
+      render :new
+    end
+  end
+
+  def destroy
+    session[:current_user_id] = nil
+    flash[:notice] = "Signed out successfully!"
+    redirect_to events_path
+  end
 end
