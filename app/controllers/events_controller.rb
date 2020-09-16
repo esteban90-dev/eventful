2cdf427489df3f77d94f79c6d776b52a1ef0b432
Stephen Allard <stephencallard@gmail.com>
2020-09-16 17:22:22 -0500

    Add #new and #create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,5 +3,27 @@ class EventsController < ApplicationController
   def index
     @events = Event.all 
   end
-  
+
+  def new
+    @event = Event.new
+    @user_options = User.all.map{ |u| [ u.name, u.id ]}
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.host = current_user
+    if @event.save
+      flash[:notice] = "Event successfully created!"
+      redirect_to events_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :location, :time, attendee_ids: [] )
+  end
+
 end
