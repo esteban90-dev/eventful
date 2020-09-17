fc209270bc9570355ea2df51ba7802b64104cf98
Stephen Allard <stephencallard@gmail.com>
2020-09-17 17:14:21 -0500

    Add model validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   belongs_to :host, class_name: "User"
   has_many :invitations
   has_many :attendees, through: :invitations
+
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :time, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   has_many :hosted_events, foreign_key: "host_id", class_name: "Event"
   has_many :invitations, foreign_key: "attendee_id"
   has_many :attending_events, through: :invitations, source: "event"
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 end
