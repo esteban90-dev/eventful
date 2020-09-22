83d7b55966518e058434e43bbc3f148954911bed
Stephen Allard <stephencallard@gmail.com>
2020-09-21 19:57:51 -0500

    Add #edit, #update, #destroy, and #find_event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,11 @@
 class EventsController < ApplicationController
+  before_action :find_event, only: [:show, :edit, :update, :destroy]
 
   def index
     @events = Event.all 
   end
 
   def show
-    @event = Event.find(params[:id])
   end
 
   def new
@@ -24,6 +24,27 @@ class EventsController < ApplicationController
       render :new
     end
   end
+  
+  def edit
+    user_options
+  end
+
+  def update
+    if @event.update(event_params)
+      flash[:notice] = "Event successfully updated"
+      redirect_to events_path
+    else
+      user_options
+      render :edit
+    end
+  end
+
+  def destroy
+    @event.destroy
+    flash[:notice] = "Event sucessfully destroyed"
+    redirect_to events_path
+  end
+
 
   private
 
@@ -33,5 +54,9 @@ class EventsController < ApplicationController
 
   def user_options
     @user_options = User.all.map{ |u| [ u.name, u.id ]}
+  end
+
+  def find_event
+    @event = Event.find(params[:id])
   end
 end
