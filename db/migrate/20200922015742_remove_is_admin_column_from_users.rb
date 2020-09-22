7db4aaf740a696c182742ac5f4b7b06a83b807bd
Stephen Allard <stephencallard@gmail.com>
2020-09-21 20:59:54 -0500

    Remove is_admin? column from users table, migrate db

---
+++ db/migrate/20200922015742_remove_is_admin_column_from_users.rb
@@ -0,0 +1,5 @@
+class RemoveIsAdminColumnFromUsers < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :users, :is_admin?
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_12_142638) do
+ActiveRecord::Schema.define(version: 2020_09_22_015742) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2020_09_12_142638) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.boolean "is_admin?"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
