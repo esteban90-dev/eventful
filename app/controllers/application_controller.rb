4b6f0f4e0cb843240193e7a3acae701414f049dc
Stephen Allard <stephencallard@gmail.com>
2020-09-21 21:24:36 -0500

    Add #current_user_id, #logged_in?, and #require_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,21 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(id: session[:current_user_id])
   end
 
+  def current_user_id
+    session[:current_user_id]
+  end
+
+  def logged_in?
+    return true if current_user
+    false
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:notice] = "You must be logged in to perform this action"
+      redirect_to new_session_path
+    end
+  end
+
   helper_method :current_user
 end
